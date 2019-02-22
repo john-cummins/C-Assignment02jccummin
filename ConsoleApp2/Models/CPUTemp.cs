8cd67aae49146f3d1065fecbd93c0ced1e6b7f14
John Cummins <jccummin@lakeheadu.ca>
2019-02-21 23:02:08 -0500

    Add project files.

---
+++ ConsoleApp2/Models/CPUTemp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace Assignment02
+{
+
+    /// <summary>
+    /// A public list that allows users to take their own computer's temperature. 
+    /// This does not output anythingn to the console, but instead just generates the current temperature when activated.
+    /// It does also turn it into a useable Celius measurement.
+    /// 
+    /// Unfortunately, it is non-fuctional due ti a permission error.
+    /// 
+    /// Parts of code taken from Lasse Rasch and JYelton, 2010, 2015, at 
+    /// https://stackoverflow.com/questions/1195112/how-to-get-cpu-temperature
+    /// </summary>
+
+    public class CPUTemp
+    {
+
+        public double CurrentValue { get; set; }
+        public string RequestedName { get; set; }
+
+        public static List<CPUTemp> Temperatures
+        {
+            get
+            {
+                List<CPUTemp> result = new List<CPUTemp>();
+
+                ManagementObjectSearcher gettingTemp = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+                foreach (ManagementObject obj in gettingTemp.Get())
+                {
+                    Double temp = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                    temp = (temp - 2732) / 10.0;
+
+                    result.Add(new CPUTemp { CurrentValue = temp, InstanceName = obj["InstanceName"].ToString() });
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Management;
+
+namespace Assignment02
+{
+    class Program
+    {
+        static Timer timer;
+
+
+        static void Main(string[] args)
+        {
+            timer = new Timer()
+            {
+                Interval = 5000, //5 second intervals
+                AutoReset = true, //Instructs the Timer to execute the event repeatedly
+                Enabled = true //Enable the Timer
+            };
+
+            timer.Elapsed += Timer_Elapsed;
+
+            Console.ReadKey();
+
+        }
+
+
+
+        ///<summary>
+        /// This event activats when the timer passes a tick, writing out both GPS and CPU Temperatures and sending them to CSV files.
+        ///</summary>
+
+        private static void Timer_Elapsed(object source, ElapsedEventArgs e)
+        {
+
+            Console.WriteLine(CPUTemp.Temperatures);            
+        }
+    }
+}
